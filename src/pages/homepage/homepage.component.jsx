eb11a42cebd548a2d0ae7d02e5ebb239fb0555e9
Helen Goatly <hgoatly@hotmail.com>
2021-07-26 16:37:41 +0100

    Break homepage into smaller components. Finish styling menu components.

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import { DEFAULT_EXTENSIONS } from '@babel/core';
+import React from 'react';
+
+import Directory from '../../components/directory/directory.component';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+);
+
+export default HomePage;
